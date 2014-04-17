5e94032c92076d71a22181dca083d64554ac3075
David Gomes <davidrafagomes@gmail.com>
2014-04-17 20:29:16 +0100

    Code style fixes.

--- server/publications.js
+++ server/publications.js
@@ -1,14 +1,14 @@
 Meteor.publish('rooms', function() {
-  return Rooms.find({}, { fields: { title: true, users: true, hostName: true,
+  return Rooms.find({ }, { fields: { title: true, users: true, hostName: true,
                                     difficulty: true, status: true } });
 });
 
-Meteor.publish('usersLSub', function(){
+Meteor.publish('usersLSub', function() {
   return Meteor.users.find({ "status.online": true },
                            { fields: { username: true, score: true, ranking: true } });
 });
 
-Meteor.publish('ownUser', function(){
+Meteor.publish('ownUser', function() {
   return Meteor.users.find(this.userId, { fields: { roomId: true } });
 });
 
